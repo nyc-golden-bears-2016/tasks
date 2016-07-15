3efe87479506d616951777ea7a762f18706d354e
Sam Blackman <sblackman@gmail.com>
2016-07-15 15:17:49 -0400

    add comments to alternative AR ways

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,8 +4,8 @@ end
 
 get '/tasks' do
   redirect '/login' unless logged_in?
-  # @tasks = Task.where(user: current_user).order(:created_at)
   @tasks = current_user.tasks.order(:created_at)
+  # @tasks = Task.where(user: current_user).order(:created_at) # the version Alex suggested which also works
   erb :'/tasks/index'
 end
 
@@ -18,7 +18,7 @@ end
 post '/tasks' do
   halt(401, "must be logged in. stop trying to hack us") unless logged_in?
   @task = Task.new(params[:task].merge(user: current_user))
-  # @task.user = current_user
+  # @task.user = current_user # alternative way to assign the current user to this task
   if @task.save
     redirect "/tasks"
   else
